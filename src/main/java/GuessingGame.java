4c72a8b96ad3d5f6954302e49f009c47e30a0441
Trevor Davis <tzendou@gmail.com>
2017-09-08 21:02:08 -0700

    add game, jar, prompter

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,6 +1,14 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        System.out.println("test");
+
+        Game game = new Game();
+        game.start();
+        Prompter prompter = new Prompter(game, game.getJar());
+        prompter.start();
+//        while (!game.isWon()) {
+//            prompter.displayProgress();
+//            prompter.promptForGuess();
+//        }
 
     }
 }
